008d058bc130cd1076cb18bbbd192e07434ebd16
santty04 <sarango844@misena.edu.co>
2023-03-13 15:59:36 -0500

    Funcionalidad de mover el jugador (Se puede salir del mapa)

--- Javascript/videojuego/game.js
+++ Javascript/videojuego/game.js
@@ -1,56 +1,109 @@
-const canvas = document.querySelector('#game');
-const game = canvas.getContext('2d');
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize; // = (canvasSize / 10 ) - 1
 
-window.addEventListener('load', setCanvasSize);
-window.addEventListener('resize', setCanvasSize);
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
 function setCanvasSize() {
-    
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
-    } else {
-        canvasSize = window.innerHeight * 0.8
-    }
-    
-    canvas.setAttribute('width', canvasSize);
-    canvas.setAttribute('height', canvasSize);
-    
-    elementsSize = (canvasSize / 10 ) - 1;
-    
-    startGame();
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  elementsSize = canvasSize / 10 - 1;
+
+  startGame();
 }
 function startGame() {
-    
-    
-    game.font = elementsSize + 'px Verdana';
-    game.textAlign = '';
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "";
 
-    const map = maps[0];
-    const mapRows = map.trim().split('\n');
-    mapRowsCols = mapRows.map(row => row.trim().split(''))
-    console.log({map, mapRows});
-    
-    mapRowsCols.forEach((row, rowI) => {
-        row.forEach((col, colI) => {
-            const emoji = emojis[col];
-            const posX = elementsSize * colI;
-            const posY = elementsSize * (rowI + 1);
-            game.fillText(emoji, posX, posY);
-            console.log( { row, rowI, col, colI } )
-        })
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  mapRowsCols = mapRows.map((row) => row.trim().split(""));
+  console.log({ map, mapRows });
+
+  game.clearRect(0, 0, canvasSize, canvasSize);
+  mapRowsCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * colI;
+      const posY = elementsSize * (rowI + 1);
+
+      if (col == "O") {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posX;
+          playerPosition.y = posY;
+          console.log({ playerPosition });
+        }
+      }
+      game.fillText(emoji, posX, posY);
     });
+  });
 
-    // for (let i = 0; i < 10; i++) {
-    //     for (let z = 1; z < 11; z++) {
-    //         game.fillText(emojis[mapRowsCols[z-1][i]], elementsSize * i, elementsSize * z);            
-    //     }
-    // }
+  movePlayer();
 
+  // for (let i = 0; i < 10; i++) {
+  //     for (let z = 1; z < 11; z++) {
+  //         game.fillText(emojis[mapRowsCols[z-1][i]], elementsSize * i, elementsSize * z);
+  //     }
+  // }
 }
+
+function movePlayer() {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveByKeys(event) {
+  if (event.key == "ArrowUp") moveUp();
+  else if (event.key == "ArrowLeft") moveLeft();
+  else if (event.key == "ArrowRight") moveRight();
+  else if (event.key == "ArrowDown") moveDown();
+}
+function moveUp() {
+  console.log("Me quiero mover hacia arriba");
+  playerPosition.y -= elementsSize;
+  startGame();
+}
+
+function moveLeft() {
+  console.log("Me quiero mover hacia izquierda");
+  playerPosition.x -= elementsSize;
+  startGame();
+}
+function moveRight() {
+  console.log("Me quiero mover hacia derecha");
+  playerPosition.x += elementsSize;
+  startGame();
+}
+function moveDown() {
+  console.log("Me quiero mover hacia abajo");
+  playerPosition.y += elementsSize;
+  startGame();
+}
+
 // window.innerHeight
 // window.innerWidth
 
@@ -60,4 +113,4 @@ function startGame() {
 // game.font = '25px Verdana';
 // game.fillStyle = 'purple';
 // game.textAlign = 'left';
-// game.fillText('Platzi', 25, 25);    
+// game.fillText('Platzi', 25, 25);
